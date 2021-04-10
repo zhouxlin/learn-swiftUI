8b70238b674eba83d29d4385fc15bf9245d166f6
linus zhou <xlin173@foxmail.com>
2021-04-10 14:38:56 +0800

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by 周学林 on 2021/4/10.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
